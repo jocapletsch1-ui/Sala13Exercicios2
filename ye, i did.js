5a6252086bffbb29556a949734d1e3ae4040a911
jocapletsch1-ui <jocapletsch1@gmail.com>
2026-05-04 10:10:51 -0300

    ye i did.js

--- ye, i did.js
+++ ye, i did.js
@@ -16,3 +16,15 @@ function dividir(){
     else
         resp.textContent = "Esse número é invalido."
 }
+function multiplicar(){
+    var n5 = document.getElementById("n5").valueAsNumber
+    var n6 = document.getElementById("n6").valueAsNumber
+    var resp = document.getElementById("respostamultiplicar")
+    resp.textContent = n5 * n6
+}
+function subtrair(){
+    var n7 = document.getElementById("n7").valueAsNumber
+    var n8 = document.getElementById("n8").valueAsNumber
+    var resp = document.getElementById("respostasubtrair")
+    resp.textContent = n7 - n8
+}
